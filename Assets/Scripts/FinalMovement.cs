76ae9a1a79ce1c0b31ce934ca5cf1b61b7c22af5
SouthBegonia <1687634916@qq.com>
2020-06-28 20:10:07 +0800

    Dash的实现

--- Assets/Scripts/FinalMovement.cs
+++ Assets/Scripts/FinalMovement.cs
@@ -9,17 +9,33 @@ public class FinalMovement : MonoBehaviour
     private Collider2D coll;
     private Animator anim;
 
-    public float speed, jumpForce;
+    [Header("跳跃参数")]
+    public float speed;
+    public float jumpForce;
     private float horizontalMove;
     public Transform groundCheck;
     public LayerMask ground;
 
-    public bool isGround, isJump, isDashing;
+    [Header("Dash参数")]
+    public float dashTime;      //dash时长
+    private float dashTimeLeft; //dash剩余时长
+    private float lastDash = -10f;     //上一次dash时间点
+    public float dashCD;        //dash CD
+    public float dashSpeed;     //dash 速度
+
+
+    [Header("Player是否处于某状态中")]
+    public bool isGround;
+    public bool isJump;
+    public bool isDashing;
 
     bool jumpPressed;
     int jumpCount;
 
-    // Start is called before the first frame update
+    [Header("UI")]
+    public Image CDImage;
+
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -27,51 +43,82 @@ public class FinalMovement : MonoBehaviour
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // 跳跃
         if (Input.GetButtonDown("Jump") && jumpCount > 0)
         {
             jumpPressed = true;
         }
+
+        // Dash
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            if(Time.time>=(lastDash+dashCD))
+            {
+                ReadyToDash();
+            }
+        }
+
+        // UI
+        CDImage.fillAmount -= 1.0f / dashCD * Time.deltaTime;
     }
 
     private void FixedUpdate()
     {
+        // 检测是否处于地面上
         isGround = Physics2D.OverlapCircle(groundCheck.position, 0.1f, ground);
 
+        // Input Dash
+        Dash();
+        if (isDashing)
+            return;
+
+        // Input移动控制
         GroundMovement();
 
+        // Input跳跃
         Jump();
 
-
+        // 动画状态切换
         SwitchAnim();
     }
 
+    /// <summary>
+    /// Input移动控制
+    /// </summary>
     void GroundMovement()
     {
+        // 改变player的速度
         horizontalMove = Input.GetAxisRaw("Horizontal");//只返回-1，0，1
         rb.velocity = new Vector2(horizontalMove * speed, rb.velocity.y);
 
+        // 改变player的方向
         if (horizontalMove != 0)
-        {
             transform.localScale = new Vector3(horizontalMove, 1, 1);
-        }
-
     }
 
-    void Jump()//跳跃
+    /// <summary>
+    /// Input跳跃
+    /// </summary>
+    void Jump()
     {
+        // 如果处于地面，则拥有2次跳跃次数
         if (isGround)
         {
             jumpCount = 2;//可跳跃数量
             isJump = false;
         }
+
+        // 如果处于地面，并按下Jump键，则进行跳跃
+        // 若处于空中，并按下Jump键，则进行二次跳跃
         if (jumpPressed && isGround)
         {
             isJump = true;
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             jumpCount--;
+
+            // 注：跳跃完重置该参数
             jumpPressed = false;
         }
         else if (jumpPressed && jumpCount > 0 && isJump)
@@ -82,8 +129,54 @@ public class FinalMovement : MonoBehaviour
         }
     }
 
-    void SwitchAnim()//动画切换
+    private void ReadyToDash()
     {
+        isDashing = true;
+        dashTimeLeft = dashTime;
+        lastDash = Time.time;
+
+        CDImage.fillAmount = 1.0f;
+    }
+
+    /// <summary>
+    /// 冲刺
+    /// </summary>
+    private void Dash()
+    {
+        if(isDashing)
+        {
+            if(dashTimeLeft>0)
+            {
+                // 起跳时dash
+                if(rb.velocity.y>0 && !isGround)
+                {
+                    rb.velocity = new Vector2(dashSpeed * horizontalMove, jumpForce);
+                }
+
+                rb.velocity = new Vector2(dashSpeed * horizontalMove, rb.velocity.y);
+
+                dashTimeLeft -= Time.deltaTime;
+
+                ShadowPool.instance.GetFromPool();
+            }
+            else
+            {
+                // dash完
+                isDashing = false;
+                if (!isGround)
+                {
+                    //rb.velocity = new Vector2(dashSpeed * horizontalMove, jumpForce);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 动画切换控制
+    /// </summary>
+    void SwitchAnim()
+    {
+        // 设置奔跑数值，范围 [0, 1*speed]
         anim.SetFloat("running", Mathf.Abs(rb.velocity.x));
 
         if (isGround)
@@ -92,10 +185,12 @@ public class FinalMovement : MonoBehaviour
         }
         else if (!isGround && rb.velocity.y > 0)
         {
+            // 当处于空中，且player有 +Y速度，说明在起跳阶段
             anim.SetBool("jumping", true);
         }
-        else if (rb.velocity.y < 0)
+        else if (!isGround && rb.velocity.y < 0)
         {
+            // 当player有 -Y速度，说明在下落阶段
             anim.SetBool("jumping", false);
             anim.SetBool("falling", true);
         }
---
+++ Assets/Scripts/ShadowSprite.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShadowSprite : MonoBehaviour
+{
+    private Transform player;
+
+    private SpriteRenderer shadowSprite;
+    private SpriteRenderer playerSprite;
+
+    private Color color;
+
+    [Header("时间控制参数")]
+    public float activeTime;
+    public float activeStart;
+
+    [Header("不透明度控制")]
+    private float alpha;
+    public float alphaSet;
+    public float alphaMultiplier;
+
+
+    private void OnEnable()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        shadowSprite = GetComponent<SpriteRenderer>();
+        playerSprite = player.GetComponent<SpriteRenderer>();
+
+        alpha = alphaSet;
+
+        shadowSprite.sprite = playerSprite.sprite;
+        transform.position = player.position;
+        transform.localScale = player.localScale;
+        transform.rotation = player.rotation;
+
+        activeStart = Time.time;
+    }
+
+    void Update()
+    {
+        // Alpha变化
+        alpha *= alphaMultiplier;
+        color = new Color(0.5f, 0.5f, 0.5f, alpha);
+        shadowSprite.color = color;
+
+        // 超时返回对象池
+        if(Time.time >=activeStart+activeTime)
+        {
+            ShadowPool.instance.ReturnPool(this.gameObject);
+        }
+    }
+}
